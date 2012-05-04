b4eeb20111cb95abd6444591b570b5d3db18652d
Arnaud Kervern <arnaud.k@gmail.com>
2012-05-05 00:06:15 +0200

    Format

--- twitisowall.js
+++ twitisowall.js
@@ -1,7 +1,7 @@
 var URI_REGEX = /https?:\/\/[-a-zA-Z0-9+&@#\/%?=~_|!:,.;]*[-a-zA-Z0-9+&@#\/%=~_|]/
 
 ;$(function() {
-    container = $("#container");
+  container = $("#container");
 
     // Init container
     container.isotope({
@@ -10,61 +10,61 @@ var URI_REGEX = /https?:\/\/[-a-zA-Z0-9+&@#\/%?=~_|!:,.;]*[-a-zA-Z0-9+&@#\/%=~_|
         layoutMode : 'masonry',
         masonry: {
             //columnWidth: 240
-        }
-    });
+          }
+        });
 
     queryTwitter("q=" + encodeURI("KohLanta"));
-});
+  });
 
 function queryTwitter(params) {
-    if (!params.match(/^\?/)) {
-        params = "?" + params
-    }
-    $.getJSON("http://search.twitter.com/search.json" + params + "&callback=?", 
-        handleResponse).error(handleError);
+  if (!params.match(/^\?/)) {
+    params = "?" + params
+  }
+  $.getJSON("http://search.twitter.com/search.json" + params + "&callback=?", 
+    handleResponse).error(handleError);
 }
 
 function handleError(data) {
-    console.log("Error")
+  console.log("Error")
 }
 
 function handleResponse(data) {
-    if (data.error) {
-        console.log("ERREUR")
-    }
+  if (data.error) {
+    console.log("ERREUR")
+  }
     //console.log(data)
     //container.empty();
     appendTweets(data)
 
     if(sessionStorage == null) {
-        console.log("Session Storage not available ...")
-        return;
+      console.log("Session Storage not available ...")
+      return;
     } else {
-        sessionStorage.refresh_url = data.refresh_url
-        startAutoUpdate();
+      sessionStorage.refresh_url = data.refresh_url
+      startAutoUpdate();
     }
 
     var nbChild = container.children().size();
     var maxItem = 50;
     if (nbChild > maxItem) {
-        container.isotope( 'remove', container.children().slice(maxItem));
+      container.isotope( 'remove', container.children().slice(maxItem));
     }
-}
+  }
 
-function appendTweets(data) {
+  function appendTweets(data) {
     $.map(data['results'], function(obj, index) {
-        var match = obj['text'].match(URI_REGEX)
-        if (match) {
-            var uri = match[0]
-            obj['text'] = obj['text'].replace(uri, '<a href="'+uri+'">' + uri + '</a>');
-        }
+      var match = obj['text'].match(URI_REGEX)
+      if (match) {
+        var uri = match[0]
+        obj['text'] = obj['text'].replace(uri, '<a href="'+uri+'">' + uri + '</a>');
+      }
     });
     var tweets = ich.tweet(data);
     container.prepend(tweets).isotope('reloadItems').isotope({ sortBy: 'original-order' })
-}
+  }
 
-function startAutoUpdate() {
+  function startAutoUpdate() {
     setTimeout(function() {
-        queryTwitter(sessionStorage.refresh_url);
+      queryTwitter(sessionStorage.refresh_url);
     }, 4000);
-}
+  }
