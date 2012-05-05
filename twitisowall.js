c400c9897ed4ec8cdc28e37ea32f6969dd29cf27
Arnaud Kervern <arnaud.k@gmail.com>
2012-05-05 06:58:53 +0200

    Format using jsFormat

--- twitisowall.js
+++ twitisowall.js
@@ -1,4 +1,5 @@
-;var URI_REGEX = /https?:\/\/[-a-zA-Z0-9+&@#\/%?=~_|!:,.;]*[-a-zA-Z0-9+&@#\/%=~_|]/
+;
+var URI_REGEX = /https?:\/\/[-a-zA-Z0-9+&@#\/%?=~_|!:,.;]*[-a-zA-Z0-9+&@#\/%=~_|]/
 var autoUpdate;
 
 $(function() {
@@ -7,8 +8,8 @@ $(function() {
   // Init container
   container.isotope({
     // options
-    itemSelector : '.item',
-    layoutMode : 'masonry',
+    itemSelector: '.item',
+    layoutMode: 'masonry',
   });
 
   //Input field
@@ -17,7 +18,7 @@ $(function() {
     clearTimeout(timeout)
     var that = $(this);
     timeout = setTimeout(function() {
-      if(sessionStorage) {
+      if (sessionStorage) {
         sessionStorage.searchInput = that.val()
       }
       search(that.val())
@@ -34,8 +35,7 @@ function queryTwitter(params) {
   if (!params.match(/^\?/)) {
     params = "?" + params
   }
-  $.getJSON("http://search.twitter.com/search.json" + params + "&callback=?", 
-    handleResponse).error(handleError);
+  $.getJSON("http://search.twitter.com/search.json" + params + "&callback=?", handleResponse).error(handleError);
 }
 
 function handleError(data) {
@@ -48,40 +48,42 @@ function handleResponse(data) {
     return;
   }
   console.log(data)
-    //console.log(data)
-    //container.empty();
-    appendTweets(data)
-    if (data.refresh_url) {
-      startAutoUpdate(data.refresh_url);
+  //console.log(data)
+  //container.empty();
+  appendTweets(data)
+  if (data.refresh_url) {
+    startAutoUpdate(data.refresh_url);
+  }
+
+  var nbChild = container.children().size();
+  var maxItem = 50;
+  if (nbChild > maxItem) {
+    container.isotope('remove', container.children().slice(maxItem));
+  }
+}
+
+function appendTweets(data) {
+  $.map(data['results'], function(obj, index) {
+    var match = obj['text'].match(URI_REGEX)
+    if (match) {
+      var uri = match[0]
+      obj['text'] = obj['text'].replace(uri, '<a href="' + uri + '">' + uri + '</a>');
     }
+  });
+  var tweets = ich.tweet(data);
+  container.prepend(tweets).isotope('reloadItems').isotope({
+    sortBy: 'original-order'
+  })
+}
 
-    var nbChild = container.children().size();
-    var maxItem = 50;
-    if (nbChild > maxItem) {
-      container.isotope( 'remove', container.children().slice(maxItem));
-    }
-  }
+function startAutoUpdate(url) {
+  autoUpdate = setTimeout(function() {
+    queryTwitter(url);
+  }, 4000);
+}
 
-  function appendTweets(data) {
-    $.map(data['results'], function(obj, index) {
-      var match = obj['text'].match(URI_REGEX)
-      if (match) {
-        var uri = match[0]
-        obj['text'] = obj['text'].replace(uri, '<a href="'+uri+'">' + uri + '</a>');
-      }
-    });
-    var tweets = ich.tweet(data);
-    container.prepend(tweets).isotope('reloadItems').isotope({ sortBy: 'original-order' })
-  }
-
-  function startAutoUpdate(url) {
-    autoUpdate = setTimeout(function() {
-      queryTwitter(url);
-    }, 4000);
-  }
-
-  function search(value) {
-    clearTimeout(autoUpdate)
-    container.isotope( 'remove', container.children());
-    queryTwitter("q=" + encodeURI(value));
-  }
+function search(value) {
+  clearTimeout(autoUpdate)
+  container.isotope('remove', container.children());
+  queryTwitter("q=" + encodeURI(value));
+}
